b4f8272b417cd91731f7415cb07251928aa2d8c8
Alex Skrypnyk <alex.designworks@gmail.com>
2026-08-18 07:18:23 +1000

    [#264] Added a tag and an environment variable to disable animated screenshots. (#266)
    * [#264] Added the '@screenshots:animated:skip' tag to opt out of animation.
    
    * [#264] Added the 'BEHAT_SCREENSHOT_ANIMATION_SKIP' variable to disable animation suite-wide.

--- tests/phpunit/Unit/ScreenshotContextAnimationTest.php
+++ tests/phpunit/Unit/ScreenshotContextAnimationTest.php
@@ -53,6 +53,62 @@ class ScreenshotContextAnimationTest extends TestCase {
       'feature animated tag' => [[], ['screenshots:animated'], [], FALSE, TRUE],
       'animation enabled via config' => [[], [], ['enabled' => TRUE], FALSE, TRUE],
       'animation disabled via config' => [[], [], ['enabled' => FALSE], FALSE, FALSE],
+      'feature animated tag over disabled config' => [[], ['screenshots:animated'], ['enabled' => FALSE], FALSE, TRUE],
+      'scenario skip tag over enabled config' => [['screenshots:animated:skip'], [], ['enabled' => TRUE], FALSE, FALSE],
+      'feature skip tag over enabled config' => [[], ['screenshots:animated:skip'], ['enabled' => TRUE], FALSE, FALSE],
+      'scenario skip tag with disabled config' => [['screenshots:animated:skip'], [], ['enabled' => FALSE], FALSE, FALSE],
+      'scenario skip tag over scenario animated tag' => [['screenshots:animated', 'screenshots:animated:skip'], [], [], FALSE, FALSE],
+      'feature skip tag over feature animated tag' => [[], ['screenshots:animated', 'screenshots:animated:skip'], [], FALSE, FALSE],
+      'scenario animated tag over feature skip tag' => [['screenshots:animated'], ['screenshots:animated:skip'], ['enabled' => FALSE], FALSE, TRUE],
+      'scenario skip tag over feature animated tag' => [['screenshots:animated:skip'], ['screenshots:animated'], ['enabled' => TRUE], FALSE, FALSE],
+    ];
+  }
+
+  #[DataProvider('dataProviderBeforeScenarioCheckScreenshotsTagHonoursSuiteEnvironmentVariable')]
+  public function testBeforeScenarioCheckScreenshotsTagHonoursSuiteEnvironmentVariable(?string $env_value, array $scenario_tags, array $feature_tags, array $animation, bool $expected_animated): void {
+    $original_value = getenv(ScreenshotContext::ENV_ANIMATION_SKIP);
+
+    try {
+      if ($env_value === NULL) {
+        putenv(ScreenshotContext::ENV_ANIMATION_SKIP);
+      }
+      else {
+        putenv(ScreenshotContext::ENV_ANIMATION_SKIP . '=' . $env_value);
+      }
+
+      $env = $this->createMock(Environment::class);
+      $feature_node = $this->createMock(FeatureNode::class);
+      $feature_node->method('hasTag')->willReturnCallback(static fn(string $tag): bool => in_array($tag, $feature_tags, TRUE));
+      $scenario = $this->createMock(ScenarioInterface::class);
+      $scenario->method('hasTag')->willReturnCallback(static fn(string $tag): bool => in_array($tag, $scenario_tags, TRUE));
+
+      $screenshot_context = new ScreenshotContext();
+      $screenshot_context->setScreenshotParameters('test-dir', TRUE, 'failed_', FALSE, FALSE, '{ext}', '{ext}', [], $animation);
+
+      $screenshot_context->beforeScenarioCheckScreenshotsTag(new BeforeScenarioScope($env, $feature_node, $scenario));
+
+      $this->assertSame($expected_animated, self::getProtectedValue($screenshot_context, 'scenarioIsAnimated'));
+    }
+    finally {
+      if ($original_value !== FALSE) {
+        putenv(ScreenshotContext::ENV_ANIMATION_SKIP . '=' . $original_value);
+      }
+      else {
+        putenv(ScreenshotContext::ENV_ANIMATION_SKIP);
+      }
+    }
+  }
+
+  public static function dataProviderBeforeScenarioCheckScreenshotsTagHonoursSuiteEnvironmentVariable(): array {
+    return [
+      'variable unset with config enabled' => [NULL, [], [], ['enabled' => TRUE], TRUE],
+      'variable set with config enabled' => ['1', [], [], ['enabled' => TRUE], FALSE],
+      'variable set with config disabled' => ['1', [], [], ['enabled' => FALSE], FALSE],
+      'variable set over scenario animated tag' => ['1', ['screenshots:animated'], [], [], FALSE],
+      'variable set over feature animated tag' => ['1', [], ['screenshots:animated'], [], FALSE],
+      'variable empty with config enabled' => ['', [], [], ['enabled' => TRUE], TRUE],
+      'variable zero with config enabled' => ['0', [], [], ['enabled' => TRUE], TRUE],
+      'variable zero with scenario animated tag' => ['0', ['screenshots:animated'], [], [], TRUE],
     ];
   }
 
