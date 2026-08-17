95932a6b57f10ac7c127bee7198ad2b3553c8334
Alex Skrypnyk <alex@drevops.com>
2026-08-17 17:14:42 +1000

    Declared the boolean config keys as boolean nodes.

--- tests/phpunit/Unit/BehatScreenshotExtensionTest.php
+++ tests/phpunit/Unit/BehatScreenshotExtensionTest.php
@@ -7,8 +7,12 @@ namespace DrevOps\BehatScreenshot\Tests\Unit;
 use DrevOps\BehatScreenshotExtension\Context\Initializer\ScreenshotContextInitializer;
 use DrevOps\BehatScreenshotExtension\ServiceContainer\BehatScreenshotExtension;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
@@ -68,6 +72,39 @@ class BehatScreenshotExtensionTest extends TestCase {
     $extension->configure($builder);
 
     $this->assertCount(10, $builder->getChildNodeDefinitions());
+  }
+
+  #[DataProvider('dataProviderConfigureAcceptsOnlyBooleans')]
+  public function testConfigureAcceptsOnlyBooleans(string $key, mixed $value, bool $is_accepted): void {
+    $tree_builder = new TreeBuilder('root');
+
+    $extension = new BehatScreenshotExtension();
+    $extension->configure($tree_builder->getRootNode());
+
+    if (!$is_accepted) {
+      $this->expectException(InvalidTypeException::class);
+    }
+
+    $processed = (new Processor())->process($tree_builder->buildTree(), [[$key => $value]]);
+
+    if ($is_accepted) {
+      $this->assertSame($value, $processed[$key]);
+    }
+  }
+
+  public static function dataProviderConfigureAcceptsOnlyBooleans(): array {
+    return [
+      'on_failed enabled' => ['on_failed', TRUE, TRUE],
+      'on_failed disabled' => ['on_failed', FALSE, TRUE],
+      'purge disabled' => ['purge', FALSE, TRUE],
+      'always_fullscreen enabled' => ['always_fullscreen', TRUE, TRUE],
+      'on_every_step enabled' => ['on_every_step', TRUE, TRUE],
+      // A quoted 'false' used to pass through as a truthy string.
+      'quoted false' => ['on_failed', 'false', FALSE],
+      'quoted true' => ['on_failed', 'true', FALSE],
+      'integer zero' => ['purge', 0, FALSE],
+      'integer one' => ['purge', 1, FALSE],
+    ];
   }
 
 }
