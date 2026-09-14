7eda3a203d0f57e10dceb611114cdd4468d9d7be
Alex Skrypnyk <alex.designworks@gmail.com>
2026-09-14 10:43:35 +1000

    [#184] Allowed Symfony 8, raised PHP floor to 8.3 and moved to PHPUnit 12. (#286)

--- rector.php
+++ rector.php
@@ -27,7 +27,7 @@ return RectorConfig::configure()
   ->withPaths([
     __DIR__ . '/**',
   ])
-  ->withPhpSets(php82: TRUE)
+  ->withPhpSets(php83: TRUE)
   ->withPreparedSets(
     deadCode: TRUE,
     codeQuality: TRUE,
